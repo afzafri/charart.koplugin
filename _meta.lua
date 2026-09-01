9db117e99ebe880b66c7df2c11893b5a070e2049
Afif Zafri <me@afifzafri.com>
2026-09-01 19:49:16 +0800

    declare a plugin version

--- _meta.lua
+++ _meta.lua
@@ -2,4 +2,5 @@ local _ = require("gettext")
 return {
     fullname = _("Character Art"),
     description = _([[Highlight a character's name while reading to see art of them, pulled from the book's wiki.]]),
+    version = "1.0.0",
 }
